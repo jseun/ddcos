20d77975f3d50c7296bb73f07d203421490e9277
Samuel Jean <sjean@idsmicronet.com>
2015-03-03 15:05:11 -0500

    Set up a local APT repository

--- scripts/bundle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-outdir=/tmp/bundle/$1; shift
-mkdir -p $outdir
-(cd $outdir; apt-get -sy install $@ | \
-	awk '/^Inst /{print $2}' | \
-	xargs apt-get download)
-ls -al $outdir | md5sum > /tmp/bundle/${1}.md5sum
