552f6e079f5b0d8e38fc8846b89ca69bd1b5636e
Samuel Jean <sjean@idsmicronet.com>
2015-03-10 13:40:22 -0400

    this commit makes it boot into docker

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 /etc/init.d/mountkernfs.sh start
+/etc/init.d/mountdevsubfs.sh start
 
-while :
-do
-  if mountpoint -q "/var"; then
-    exec docker -d -s aufs --host tcp://0.0.0.0:2375 --host unix:///var/run/docker.sock \
-      -g /var/lib/docker
-  elif [ -x "/install.sh" ]; then
-    exec /install.sh
-  else
-    echo "E: Operating system left unconfigured."
-    exec /bin/bash --norc
-  fi
-done
+ipconfig eth0
+ipaddr=$(ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}')
+
+if mountpoint -q "/var" && test -x /etc/init.d/docker; then
+  while [ ! -f /gracefull_system_reboot ];
+  do
+    if /etc/init.d/docker status >/dev/null; then
+      clear
+      docker ps 2>/dev/null && echo && \
+        echo "Docker is reachable at tcp://${ipaddr}:2375"
+    else
+      /etc/init.d/docker start
+    fi
+    sleep 5
+  done
+  /etc/init.d/docker stop
+elif [ -x "/install.sh" ]; then
+  exec /install.sh
+else
+  echo "E: Operating system left unconfigured."
+  /bin/bash --norc
+fi
+
+reboot
